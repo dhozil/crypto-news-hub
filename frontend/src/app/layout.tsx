e196e788109410ade1e6d52adfe71edde369b52c
DhoziL <55169789+dhozil@users.noreply.github.com>
2026-04-06 22:19:30 +0700

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import { WalletProvider } from '@/context/WalletProvider';
+import { WalletProvider } from '@/hooks/WalletProvider';
 
 const inter = Inter({
   subsets: ["latin"],
